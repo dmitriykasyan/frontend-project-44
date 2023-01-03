725865dd91f159ccf8d935eddc43b7e366183b73
Dmitry Kasyan <dmitriy.kasyan@gmail.com>
2023-01-03 15:03:39 +0000

    fix lint in src/ files

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const getName = (myQuestion) => {
-
   // const name = readlineSync.question('May I have your name? ');
   const name = readlineSync.question(myQuestion);
-  
+
   return `Hello, ${name}!`;
 };
 
-export { getName };
-// console.log('Test!!!');
-// console.log(getName('May I have your name? '));
+export default { getName };
--- src/index.js
+++ src/index.js
@@ -1,47 +1,55 @@
 import readlineSync from 'readline-sync';
 
 const getAnswer = (str) => {
-  return readlineSync.question(str);
+  const sting = readlineSync.question(str);
+  return sting;
 };
 
 const getRandomInt = (min = 0, max = 99) => {
-  return Math.floor(Math.random() * (max - min) + min);
+  const rndInt = Math.floor(Math.random() * (max - min) + min);
+  return rndInt;
 };
 
 const getArithExp = (num1, num2, num3) => {
-//  console.log(num1, num2, num3)
   switch (num3) {
     case 0:
-      console.log(`Question: ${num1} + ${num2}`)
-      return num1 + num2 ;
+      console.log(`Question: ${num1} + ${num2}`);
+      return num1 + num2;
     case 1:
-      console.log(`Question: ${num1} - ${num2}`)
-      return  num1 - num2 ;
+      console.log(`Question: ${num1} - ${num2}`);
+      return num1 - num2;
     case 2:
-      console.log(`Question: ${num1} * ${num2}`)
-      return  num1 * num2 ;
+      console.log(`Question: ${num1} * ${num2}`);
+      return num1 * num2;
+    // no default
   }
+  return null;
 };
 
 const getDelimiterArray = (num) => {
   // console.log('number',num);
   const res = [];
-  for (let i = 1; i <= num; i++ ) {
+  for (let i = 1; i <= num; i += 1) {
     if ((num % i) === 0) {
       res.push(i);
     }
   }
-  // console.log('delimeters = ',res);
   return res;
 };
 
 const getProgression = (start, step, elem = 10) => {
   const resProgression = [];
-  let a = start;
-  for (let i = 1; i <= elem; i++ ) {
-    resProgression.push(a + ((i-1)*step));
+  const a = start;
+  for (let i = 1; i <= elem; i += 1) {
+    resProgression.push(a + ((i - 1) * step));
   }
   return resProgression;
 };
 
-export {getAnswer, getArithExp, getRandomInt, getDelimiterArray, getProgression};
+export {
+  getAnswer,
+  getArithExp,
+  getRandomInt,
+  getDelimiterArray,
+  getProgression,
+};
